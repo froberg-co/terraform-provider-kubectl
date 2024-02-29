c0d108682d22bd30d46354f665b5df4c0e29d6e3
Danny Froberg <danny@froberg.org>
2024-02-29 12:38:09 +0100

    fix: rename to test or test fails

--- _examples/secrets/dynamic_secret.tf
+++ _examples/secrets/dynamic_secret.tf
@@ -17,7 +17,7 @@ locals {
 #   }
 # }
 
-resource "kubectl_manifest" "test_mock" {
+resource "kubectl_manifest" "test" {
   sensitive_fields = ["stringData"]
   apply_only       = true
   force_new        = true
@@ -57,44 +57,3 @@ resource "kubectl_manifest" "test_mock" {
     }
   })
 }
-# resource "kubectl_manifest" "argocd_applications_credentials_bare_secrets_mock" {
-#   sensitive_fields = ["stringData"]
-#   apply_only       = true
-#   force_new        = true
-#   for_each         = { for k, v in var.mock_data : k => v if try(v.ssh_key_secret_name, null) != null }
-
-#   yaml_body = yamlencode({
-#     apiVersion = "v1"
-#     kind       = "Secret"
-#     type       = "Opaque"
-#     metadata = {
-#       name      = "name-here-${each.key}-repo-secret"
-#       namespace = local.argocd_namespace
-#       annotations = {
-#         "managed-by" : "argocd.argoproj.io"
-#         "argocd.argoproj.io/compare-options" : "IgnoreExtra"
-#         "argocd.argoproj.io/sync-options" : "Prune=false"
-#       }
-#       labels = {
-#         "argocd.argoproj.io/secret-type" : each.key == "generic" ? "repo-creds" : "repository"
-#         "app.kubernetes.io/part-of" : "argocd"
-#         "app.kubernetes.io/name" : "argocd-secret"
-#       }
-#     }
-
-#     stringData = length(regexall("git@", each.value.repo_url)) > 0 ? {
-#       name          = "${each.key}-repo"
-#       insecure      = tostring(lookup(each.value, "insecure", false))
-#       sshPrivateKey = "mock-ssh-private-key"
-#       type          = "git"
-#       url           = each.value.repo_url
-#       } : {
-#       name     = "${each.key}-repo"
-#       insecure = tostring(lookup(each.value, "insecure", false))
-#       username = "mock-username"
-#       password = "mock-password"
-#       type     = "git"
-#       url      = each.value.repo_url
-#     }
-#   })
-# }
